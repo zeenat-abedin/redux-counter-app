d12256c4223b6115be08d005a3de9d1aac24f21c
zeenat <zeenu.23@gmail.com>
2024-02-19 10:19:45 +0530

    Update Counter component styles and remove unused CSS file

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Counter.css';
 
 export default function Counter() {
     return (
         <div className="counter_app">
             <h1>Counter App</h1>
             <div className="counter">
-                <button>Increment</button> {0}
-                <button>Decrement</button>
+                <button className="increment">Increment</button> {0}
+                <button className="decrement">Decrement</button>
             </div>
       </div>
   )
